b1f0f08e92b9fa1b432084db16f31f4664d14844
周广亚 <guangya.zhou@longbridge-inc.com>
2026-04-09 23:25:17 +0800

    format code；

--- packet.c
+++ packet.c
@@ -1,158 +1,139 @@
 ﻿#include "packet.h"
 
-int packet_queue_init(packet_queue_t *q)
-{
-    memset(q, 0, sizeof(packet_queue_t));
-    q->mutex = SDL_CreateMutex();
-    if (!q->mutex)
-    {
-        av_log(NULL, AV_LOG_FATAL, "SDL_CreateMutex(): %s\n", SDL_GetError());
-        return AVERROR(ENOMEM);
-    }
-    q->cond = SDL_CreateCond();
-    if (!q->cond)
-    {
-        av_log(NULL, AV_LOG_FATAL, "SDL_CreateCond(): %s\n", SDL_GetError());
-        return AVERROR(ENOMEM);
-    }
-    q->abort_request = 0;
-    return 0;
+int packet_queue_init(packet_queue_t *q) {
+  memset(q, 0, sizeof(packet_queue_t));
+  q->mutex = SDL_CreateMutex();
+  if (!q->mutex) {
+    av_log(NULL, AV_LOG_FATAL, "SDL_CreateMutex(): %s\n", SDL_GetError());
+    return AVERROR(ENOMEM);
+  }
+  q->cond = SDL_CreateCond();
+  if (!q->cond) {
+    av_log(NULL, AV_LOG_FATAL, "SDL_CreateCond(): %s\n", SDL_GetError());
+    return AVERROR(ENOMEM);
+  }
+  q->abort_request = 0;
+  return 0;
 }
 
-
 // 写队列尾部。pkt是一包还未解码的音频数据
-int packet_queue_put(packet_queue_t *q, AVPacket *pkt)
-{
-    packet_listnode_t *pkt_listnode;
+int packet_queue_put(packet_queue_t *q, AVPacket *pkt) {
+  packet_listnode_t *pkt_listnode;
 
-    pkt_listnode = av_malloc(sizeof(packet_listnode_t));
-    if (!pkt_listnode)
-    {
-        return -1;
-    }
-    pkt_listnode->pkt = av_packet_alloc();
-    if (!pkt_listnode->pkt)
-    {
-        av_free(pkt_listnode);
-        return -1;
-    }
-    
-    av_packet_move_ref(pkt_listnode->pkt, pkt);
-    pkt_listnode->next = NULL;
+  pkt_listnode = av_malloc(sizeof(packet_listnode_t));
+  if (!pkt_listnode) {
+    return -1;
+  }
+  pkt_listnode->pkt = av_packet_alloc();
+  if (!pkt_listnode->pkt) {
+    av_free(pkt_listnode);
+    return -1;
+  }
 
-    SDL_LockMutex(q->mutex);
+  av_packet_move_ref(pkt_listnode->pkt, pkt);
+  pkt_listnode->next = NULL;
 
-    if (!q->last_pkt)   // 队列为空
-    {
-        q->first_pkt = pkt_listnode;
-    }
-    else
-    {
-        q->last_pkt->next = pkt_listnode;
-    }
-    q->last_pkt = pkt_listnode;
-    q->nb_packets++;
-    q->size += pkt_listnode->pkt->size;
-    // 发个条件变量的信号：重启等待q->cond条件变量的一个线程
-    SDL_CondSignal(q->cond);
+  SDL_LockMutex(q->mutex);
 
-    SDL_UnlockMutex(q->mutex);
-    return 0;
+  if (!q->last_pkt)  // 队列为空
+  {
+    q->first_pkt = pkt_listnode;
+  } else {
+    q->last_pkt->next = pkt_listnode;
+  }
+  q->last_pkt = pkt_listnode;
+  q->nb_packets++;
+  q->size += pkt_listnode->pkt->size;
+  // 发个条件变量的信号：重启等待q->cond条件变量的一个线程
+  SDL_CondSignal(q->cond);
+
+  SDL_UnlockMutex(q->mutex);
+  return 0;
 }
 
 // 读队列头部。
-int packet_queue_get(packet_queue_t *q, AVPacket *pkt, int block)
-{
-    packet_listnode_t *p_pkt_node;
-    int ret;
+int packet_queue_get(packet_queue_t *q, AVPacket *pkt, int block) {
+  packet_listnode_t *p_pkt_node;
+  int ret;
 
-    SDL_LockMutex(q->mutex);
+  SDL_LockMutex(q->mutex);
 
-    while (1)
+  while (1) {
+    p_pkt_node = q->first_pkt;
+    if (p_pkt_node)  // 队列非空，取一个出来
     {
-        p_pkt_node = q->first_pkt;
-        if (p_pkt_node)             // 队列非空，取一个出来
-        {
-            q->first_pkt = p_pkt_node->next;
-            if (!q->first_pkt)
-            {
-                q->last_pkt = NULL;
-            }
-            q->nb_packets--;
-            q->size -= p_pkt_node->pkt->size;
-            av_packet_move_ref(pkt, p_pkt_node->pkt);
-            av_packet_free(&p_pkt_node->pkt);
-            av_free(p_pkt_node);
-            ret = 1;
-            break;
-        }
-        else if (!block)            // 队列空且阻塞标志无效，则立即退出
-        {
-            ret = 0;
-            break;
-        }
-        else                        // 队列空且阻塞标志有效，则等待
-        {
-            // SDL_CondWait(q->cond, q->mutex);
-            int signaled = 1;
-            while (signaled && !q->abort_request) {
-                signaled = SDL_CondWaitTimeout(q->cond, q->mutex, 20);
-            }
-            if (q->abort_request) {
-                SDL_UnlockMutex(q->mutex);
-                return -1;
-            }
-        }
+      q->first_pkt = p_pkt_node->next;
+      if (!q->first_pkt) {
+        q->last_pkt = NULL;
+      }
+      q->nb_packets--;
+      q->size -= p_pkt_node->pkt->size;
+      av_packet_move_ref(pkt, p_pkt_node->pkt);
+      av_packet_free(&p_pkt_node->pkt);
+      av_free(p_pkt_node);
+      ret = 1;
+      break;
+    } else if (!block)  // 队列空且阻塞标志无效，则立即退出
+    {
+      ret = 0;
+      break;
+    } else  // 队列空且阻塞标志有效，则等待
+    {
+      // SDL_CondWait(q->cond, q->mutex);
+      int signaled = 1;
+      while (signaled && !q->abort_request) {
+        signaled = SDL_CondWaitTimeout(q->cond, q->mutex, 20);
+      }
+      if (q->abort_request) {
+        SDL_UnlockMutex(q->mutex);
+        return -1;
+      }
     }
-    SDL_UnlockMutex(q->mutex);
-    return ret;
+  }
+  SDL_UnlockMutex(q->mutex);
+  return ret;
 }
 
-int packet_queue_put_nullpacket(packet_queue_t *q, int stream_index)
-{
-    // just alloc in stack
-    AVPacket *pkt = av_packet_alloc();
-    pkt->data = NULL;
-    pkt->size = 0;
-    pkt->stream_index = stream_index;
-    int ret = packet_queue_put(q, pkt);
-    av_packet_free(&pkt);
-    return ret;
+int packet_queue_put_nullpacket(packet_queue_t *q, int stream_index) {
+  // just alloc in stack
+  AVPacket *pkt = av_packet_alloc();
+  pkt->data = NULL;
+  pkt->size = 0;
+  pkt->stream_index = stream_index;
+  int ret = packet_queue_put(q, pkt);
+  av_packet_free(&pkt);
+  return ret;
 }
 
-void packet_queue_flush(packet_queue_t *q)
-{
-    packet_listnode_t *pkt, *pkt1;
+void packet_queue_flush(packet_queue_t *q) {
+  packet_listnode_t *pkt, *pkt1;
 
-    SDL_LockMutex(q->mutex);
-    for (pkt = q->first_pkt; pkt; pkt = pkt1) {
-        pkt1 = pkt->next;
-        av_packet_free(&pkt->pkt);
-        av_free(pkt);
-    }
-    q->last_pkt = NULL;
-    q->first_pkt = NULL;
-    q->nb_packets = 0;
-    q->size = 0;
-    q->duration = 0;
-    SDL_UnlockMutex(q->mutex);
+  SDL_LockMutex(q->mutex);
+  for (pkt = q->first_pkt; pkt; pkt = pkt1) {
+    pkt1 = pkt->next;
+    av_packet_free(&pkt->pkt);
+    av_free(pkt);
+  }
+  q->last_pkt = NULL;
+  q->first_pkt = NULL;
+  q->nb_packets = 0;
+  q->size = 0;
+  q->duration = 0;
+  SDL_UnlockMutex(q->mutex);
 }
 
-void packet_queue_destroy(packet_queue_t *q)
-{
-    packet_queue_flush(q);
-    SDL_DestroyMutex(q->mutex);
-    SDL_DestroyCond(q->cond);
+void packet_queue_destroy(packet_queue_t *q) {
+  packet_queue_flush(q);
+  SDL_DestroyMutex(q->mutex);
+  SDL_DestroyCond(q->cond);
 }
 
-void packet_queue_abort(packet_queue_t *q)
-{
-    SDL_LockMutex(q->mutex);
+void packet_queue_abort(packet_queue_t *q) {
+  SDL_LockMutex(q->mutex);
 
-    q->abort_request = 1;
+  q->abort_request = 1;
 
-    SDL_CondSignal(q->cond);
+  SDL_CondSignal(q->cond);
 
-    SDL_UnlockMutex(q->mutex);
+  SDL_UnlockMutex(q->mutex);
 }
-
--- player.h
+++ player.h
@@ -59,14 +59,14 @@
 #define FF_QUIT_EVENT    (SDL_USEREVENT + 2)
 
 typedef struct {
-    double pts;                     // 当前帧(待播放)显示时间戳，播放后，当前帧变成上一帧
-    double pts_drift;               // 当前帧显示时间戳与当前系统时钟时间的差值
-    double last_updated;            // 当前时钟(如视频时钟)最后一次更新时间，也可称当前时钟时间
-    double speed;                   // 时钟速度控制，用于控制播放速度
-    int serial;                     // 播放序列，所谓播放序列就是一段连续的播放动作，一个seek操作会启动一段新的播放序列
-    int paused;                     // 暂停标志
-    int *queue_serial;              // 指向packet_serial
-}   play_clock_t;
+    double pts; // 当前帧(待播放)显示时间戳，播放后，当前帧变成上一帧
+    double pts_drift; // 当前帧显示时间戳与当前系统时钟时间的差值
+    double last_updated; // 当前时钟(如视频时钟)最后一次更新时间，也可称当前时钟时间
+    double speed; // 时钟速度控制，用于控制播放速度
+    int serial; // 播放序列，所谓播放序列就是一段连续的播放动作，一个seek操作会启动一段新的播放序列
+    int paused; // 暂停标志
+    int *queue_serial; // 指向packet_serial
+} play_clock_t;
 
 typedef struct {
     int freq;
@@ -75,10 +75,10 @@ typedef struct {
     enum AVSampleFormat fmt;
     int frame_size;
     int bytes_per_sec;
-}   audio_param_t;
+} audio_param_t;
 
 typedef struct {
-    SDL_Window *window; 
+    SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
     SDL_Rect rect;
@@ -89,7 +89,7 @@ typedef struct {
     int width;
     int height;
     double height_width_ratio;
-}   sdl_video_t;
+} sdl_video_t;
 
 typedef struct packet_listnode {
     AVPacket *pkt;
@@ -98,42 +98,42 @@ typedef struct packet_listnode {
 
 typedef struct packet_queue_t {
     packet_listnode_t *first_pkt, *last_pkt;
-    int nb_packets;                 // 队列中packet的数量
-    int size;                       // 队列所占内存空间大小
-    int64_t duration;               // 队列中所有packet总的播放时长
+    int nb_packets; // 队列中packet的数量
+    int size; // 队列所占内存空间大小
+    int64_t duration; // 队列中所有packet总的播放时长
     int abort_request;
-    int serial;                     // 播放序列，所谓播放序列就是一段连续的播放动作，一个seek操作会启动一段新的播放序列
+    int serial; // 播放序列，所谓播放序列就是一段连续的播放动作，一个seek操作会启动一段新的播放序列
     SDL_mutex *mutex;
     SDL_cond *cond;
-}   packet_queue_t;
+} packet_queue_t;
 
 /* Common struct for handling all types of decoded data and allocated render buffers. */
 typedef struct {
     AVFrame *frame;
     int serial;
-    double pts;           /* presentation timestamp for the frame */
-    double duration;      /* estimated duration of the frame */
-    int64_t pos;                    // frame对应的packet在输入文件中的地址偏移
+    double pts; /* presentation timestamp for the frame */
+    double duration; /* estimated duration of the frame */
+    int64_t pos; // frame对应的packet在输入文件中的地址偏移
     int width;
     int height;
     int format;
     AVRational sar;
     int uploaded;
     int flip_v;
-}   frame_t;
+} frame_t;
 
 typedef struct {
     frame_t queue[FRAME_QUEUE_SIZE];
-    int rindex;                     // 读索引。待播放时读取此帧进行播放，播放后此帧成为上一帧
-    int windex;                     // 写索引
-    int size;                       // 总帧数
-    int max_size;                   // 队列可存储最大帧数
+    int rindex; // 读索引。待播放时读取此帧进行播放，播放后此帧成为上一帧
+    int windex; // 写索引
+    int size; // 总帧数
+    int max_size; // 队列可存储最大帧数
     int keep_last;
-    int rindex_shown;               // 当前是否有帧在显示
+    int rindex_shown; // 当前是否有帧在显示
     SDL_mutex *mutex;
     SDL_cond *cond;
-    packet_queue_t *pktq;           // 指向对应的packet_queue
-}   frame_queue_t;
+    packet_queue_t *pktq; // 指向对应的packet_queue
+} frame_queue_t;
 
 typedef struct {
     char *filename;
@@ -147,8 +147,8 @@ typedef struct {
     int video_idx;
     sdl_video_t sdl_video;
 
-    play_clock_t audio_clk;                   // 音频时钟
-    play_clock_t video_clk;                   // 视频时钟
+    play_clock_t audio_clk; // 音频时钟
+    play_clock_t video_clk; // 视频时钟
     double frame_timer;
 
     packet_queue_t audio_pkt_queue;
@@ -162,13 +162,13 @@ typedef struct {
 
     audio_param_t audio_param_src;
     audio_param_t audio_param_tgt;
-    int audio_hw_buf_size;              // SDL音频缓冲区大小(单位字节)
-    uint8_t *p_audio_frm;               // 指向待播放的一帧音频数据，指向的数据区将被拷入SDL音频缓冲区。若经过重采样则指向audio_frm_rwr，否则指向frame中的音频
-    uint8_t *audio_frm_rwr;             // 音频重采样的输出缓冲区
-    unsigned int audio_frm_size;        // 待播放的一帧音频数据(audio_buf指向)的大小
-    unsigned int audio_frm_rwr_size;    // 申请到的音频缓冲区audio_frm_rwr的实际尺寸
-    int audio_cp_index;                 // 当前音频帧中已拷入SDL音频缓冲区的位置索引(指向第一个待拷贝字节)
-    int audio_write_buf_size;           // 当前音频帧中尚未拷入SDL音频缓冲区的数据量，audio_frm_size = audio_cp_index + audio_write_buf_size
+    int audio_hw_buf_size; // SDL音频缓冲区大小(单位字节)
+    uint8_t *p_audio_frm; // 指向待播放的一帧音频数据，指向的数据区将被拷入SDL音频缓冲区。若经过重采样则指向audio_frm_rwr，否则指向frame中的音频
+    uint8_t *audio_frm_rwr; // 音频重采样的输出缓冲区
+    unsigned int audio_frm_size; // 待播放的一帧音频数据(audio_buf指向)的大小
+    unsigned int audio_frm_rwr_size; // 申请到的音频缓冲区audio_frm_rwr的实际尺寸
+    int audio_cp_index; // 当前音频帧中已拷入SDL音频缓冲区的位置索引(指向第一个待拷贝字节)
+    int audio_write_buf_size; // 当前音频帧中尚未拷入SDL音频缓冲区的数据量，audio_frm_size = audio_cp_index + audio_write_buf_size
     double audio_clock;
     int audio_clock_serial;
 
@@ -183,16 +183,18 @@ typedef struct {
     int step;
 
     SDL_cond *continue_read_thread;
-    SDL_Thread *read_tid;           // demux解复用线程
+    SDL_Thread *read_tid; // demux解复用线程
     SDL_Thread *video_dec_tid;
     SDL_Thread *audio_dec_tid;
     SDL_Thread *video_ply_tid;
-
-}   player_stat_t;
+} player_stat_t;
 
 int player_running(const char *p_input_file);
+
 double get_clock(play_clock_t *c);
+
 void set_clock_at(play_clock_t *c, double pts, int serial, double time);
+
 void set_clock(play_clock_t *c, double pts, int serial);
 
 #endif
