8547d3a7075f7621e00e01d976236b54d85a179d
周广亚 <guangya.zhou@longbridge-inc.com>
2026-04-18 13:10:42 +0800

    TAG : delete all original code for starting myself code.

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
-#include "player.h"
-
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        av_log(NULL, AV_LOG_FATAL, "Please provide a movie file, usage: \n./simpleFFplay test.mp4\n");
-        return -1;
-    }
-    player_running(argv[1]);
+    printf("Hello.\n");
+    printf("I start to discover ffplay on 2026.04.18\n");
 
     return 0;
 }
